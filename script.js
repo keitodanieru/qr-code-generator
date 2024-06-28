f48d17fce97bd1f1d339545fa7d82bf6b1c71158
keitodanieru <katedanielle03@gmail.com>
2024-06-28 23:24:05 +0800

    still some bugs in qr generation css

---
+++ script.js
@@ -0,0 +1,7 @@
+let imageBox = document.getElementById("imageBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+
+function generateQR(){
+    qrImage.src ="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example" & qrText.value;
+}
